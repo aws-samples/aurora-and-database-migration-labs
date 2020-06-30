c40c921c13fcc56a0b3567ed07d386aacfa7fb3b
Arabinda Pani <arabindp@amazon.com>
2020-06-30 14:01:08 -0700

    Adding top CPU analysis query

--- Code/PGPerfStatsSnapper/SQLs/snappermenu.sql
+++ Code/PGPerfStatsSnapper/SQLs/snappermenu.sql
@@ -38,6 +38,7 @@
 \qecho ' '
 \qecho 'top_20_sqls_by_calls.sql               			Top 20 queries by Executions/Calls in a time window'
 \qecho 'top_20_sqls_by_elapsed_time.sql        			Top 20 queries by Elapsed time in a time window'
+\qecho 'top_10_sqls_by_cpu_by_snap_id.sql        		Top 10 SQL queries by CPU by Snap ID'
 \qecho 'sql_stat_history.sql                			Execution trend of a query of interest in a time window'
 \qecho ' '
 \qecho ' '
---
+++ Code/PGPerfStatsSnapper/SQLs/top_10_sqls_by_cpu_by_snap_id.sql
@@ -0,0 +1,15 @@
+with get_cpu_percentage as (
+SELECT snap_id,dbid,userid,queryid,substring(query, 1, 50) AS short_query,
+round(total_time::numeric, 2) AS total_time_ms,
+calls,
+round(mean_time::numeric, 2) AS mean_time_ms,
+round((100 * total_time / sum(total_time::numeric) OVER (partition by snap_id))::numeric, 2) AS percentage_cpu
+FROM  pg_stat_statements_history
+where snap_id between :begin_snap_id and :end_snap_id
+),
+get_cpu_percentage_rank as (
+select snap_id,dbid,userid,queryid,short_query,total_time_ms,calls,mean_time_ms,percentage_cpu,
+rank() OVER (PARTITION BY snap_id ORDER BY percentage_cpu DESC) as pos
+from get_cpu_percentage
+)
+select snap_id,dbid,userid,queryid,short_query,total_time_ms,calls,mean_time_ms,percentage_cpu from get_cpu_percentage_rank where pos <= 10;
